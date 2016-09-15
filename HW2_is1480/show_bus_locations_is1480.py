693c96fb426790c43a4b970d68fc4cd2679da0b5
Ian Stuart <iws.stuart@gmail.com>
2016-09-15 17:57:31 -0400

    change from static json data to API response

--- HW2_is1480/show_bus_locations_is1480.py
+++ HW2_is1480/show_bus_locations_is1480.py
@@ -1,22 +1,29 @@
 import sys
 import json
+import urllib2 
 
-# key = 6a40826c-4f43-4d81-834d-c20d8dc72a1d
-# key = sys.argv[1]
+mtaKey = "6a40826c-4f43-4d81-834d-c20d8dc72a1d"
+# mtaKey = sys.argv[1]
+busLine = "B52"
 
-# data = http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=B52%(key)
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(mtaKey, busLine)
 
-with open('test_data.json') as data:
-	 busDict = json.load(data)
-	 activeBusData = busDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
-	 busName = activeBusData[0]['MonitoredVehicleJourney']['PublishedLineName']
-	 busCount = 0
-	 locations = ""
-	 for i in activeBusData:
-	 	lat = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-	 	lon = i['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-	 	locations = locations + "Bus " + str(busCount) + " is at latitude " + str(lat) + " and longitude " + str(lon) + "\n"
-	 	busCount+=1
-	 print "Bus Line : " + busName
-	 print "Number of Active Buses : " + str(busCount)
-	 print locations
+response = urllib2.urlopen(url)
+data = response.read().decode("utf-8")
+busDict = json.loads(data)
+# print busDict.keys()
+
+# with open('test_data.json') as data:
+# 	 busDict = json.load(data)
+activeBusData = busDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+busName = activeBusData[0]['MonitoredVehicleJourney']['PublishedLineName']
+busCount = 0
+locations = ""
+for i in activeBusData:
+	lat = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+	lon = i['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+	locations = locations + "Bus " + str(busCount) + " is at latitude " + str(lat) + " and longitude " + str(lon) + "\n"
+	busCount+=1
+print "Bus Line : " + busName
+print "Number of Active Buses : " + str(busCount)
+print locations
