26e946cade839f1f62753479784426c3f691134a
Ian Stuart <iws.stuart@gmail.com>
2016-09-15 18:11:42 -0400

    clean up code, add comments

--- HW2_is1480/show_bus_locations_is1480.py
+++ HW2_is1480/show_bus_locations_is1480.py
@@ -4,27 +4,31 @@ import urllib2
 
 # mtaKey = "6a40826c-4f43-4d81-834d-c20d8dc72a1d"
 mtaKey = sys.argv[1]
-# busLine = "B52"
 busLine = sys.argv[2]
 
+# format response url with api key and bus line variables from command line input
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(mtaKey, busLine)
 
+# load api response data into python dict
 response = urllib2.urlopen(url)
 data = response.read().decode("utf-8")
 busDict = json.loads(data)
-# print busDict.keys()
 
-# with open('test_data.json') as data:
-# 	 busDict = json.load(data)
+# set variable to access individual bus data in nested dict structure
 activeBusData = busDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+
 busName = activeBusData[0]['MonitoredVehicleJourney']['PublishedLineName']
 busCount = 0
 locations = ""
+
+# get lat/lon for each active bus, and total count
 for i in activeBusData:
 	lat = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
 	lon = i['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
 	locations = locations + "Bus " + str(busCount) + " is at latitude " + str(lat) + " and longitude " + str(lon) + "\n"
 	busCount+=1
+
+# format output
 print "Bus Line : " + busName
 print "Number of Active Buses : " + str(busCount)
 print locations
