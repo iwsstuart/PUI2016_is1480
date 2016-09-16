a966c9ed2142e9828e951d4ee206d43dd44a41c3
Ian Stuart <iws.stuart@gmail.com>
2016-09-16 16:35:13 -0400

    take command line inputs

--- HW2_is1480/get_bus_info_is1480.py
+++ HW2_is1480/get_bus_info_is1480.py
@@ -3,17 +3,17 @@ import json
 import urllib2 
 import csv
 
-mtaKey = "6a40826c-4f43-4d81-834d-c20d8dc72a1d"
-# mtaKey = sys.argv[1]
-busLine = "B52"
-# busLine = sys.argv[2]
+# mtaKey = "6a40826c-4f43-4d81-834d-c20d8dc72a1d"
+mtaKey = sys.argv[1]
+# busLine = "B49"
+busLine = sys.argv[2]
 
 # format response url with api key and bus line variables from command line input
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(mtaKey, busLine)
 
 # load api response data into python dict
-response = open('test_data.json')
-# response = urllib2.urlopen(url)
+# # response = open('test_data.json')
+response = urllib2.urlopen(url)
 data = response.read().decode("utf-8")
 busDict = json.loads(data)
 
@@ -32,9 +32,10 @@ for i in activeBusData:
 	singleBus = [lat, lon, stopName, stopStatus]
 	allBuses.append(singleBus)
 
-print allBuses
-
-# # format output
-# print "Bus Line : " + busName
-# print "Number of Active Buses : " + str(busCount)
-# print locations
+# write nested array to csv
+with open(sys.argv[3], 'w') as csvFile:
+    csvData = csv.writer(csvFile)
+    headerRow = ['Latitude', 'Longitude', 'Stop Name', 'Stop Status']
+    csvData.writerow(headerRow)
+    for row in allBuses:
+        csvData.writerow(row)
